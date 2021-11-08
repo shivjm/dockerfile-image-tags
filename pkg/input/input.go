58e6c289b9494bc6f5674518c7a9f59ff9bc7194
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2021-11-08 13:59:48 +0530

    feat(input)!: allow passing `-` as filename for STDIN

--- pkg/input/input.go
+++ pkg/input/input.go
@@ -5,10 +5,17 @@ import (
 	"os"
 )
 
-// GetInput opens the file named in `args` if present, `os.Stdin` if not.
+// GetInput opens and returns the file named in `args` if present,
+// `os.Stdin` if not. The special file `-` can be passed explicitly to
+// read from `Stdin`.
 func GetInput(args []string) (*os.File, error) {
 	if len(args) == 1 {
 		name := args[0]
+
+		if name == "-" {
+			return os.Stdin, nil
+		}
+
 		f, err := os.Open(name)
 
 		if err != nil {
